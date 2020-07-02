461b5963a6323a73ccd027b7a713a2fcf5689a28
Vitalii Dvornyk <dvornikvitaliy@gmail.com>
2020-07-02 14:25:38 +0300

    Практика ч. 1

--- Js/script.js
+++ Js/script.js
@@ -21,8 +21,22 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-const numberOfFilms = +promt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
-}
+    movies: {},
+    actors: {},
+    genres: [],
+    privet: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
